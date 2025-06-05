a000b0c68db668a46a856333708d42cf9f16c394
Satyam Singh <satyamrajput7239@gmail.com>
2025-06-05 09:30:31 +0530

    fix:model and response

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  name: { type: String, required: true, unique: true },
-  time: { type: String, default: () => new Date().toISOString() },
+  name: { type: String, required: true },
+  is_active: { type: Boolean, default: true },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", UserSchema);
